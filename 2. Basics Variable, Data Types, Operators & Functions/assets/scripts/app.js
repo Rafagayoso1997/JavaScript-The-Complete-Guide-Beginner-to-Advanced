889302dc06563374bf68fc71b5e898b12d3b155c
Rafa Gayoso <gayoso0597@gmail.com>
2021-11-10 10:30:21 +0100

    JavaScript Progress

--- 2. Basics Variable, Data Types, Operators & Functions/assets/scripts/app.js
+++ 2. Basics Variable, Data Types, Operators & Functions/assets/scripts/app.js
@@ -1,5 +1,66 @@
-let currentResult = 0;
+const defaultResult = 0;
+let currentResult = defaultResult;
+let logEntries = [];
 
-currentResult = currentResult + 10;
+//Get input from input field
+function getUserNumberInput() {
+  return parseInt(usrInput.value);
+}
 
-outputResult(currentResult, '');
+//Generates and writes calculation log
+function createAndWriteOutput(operator, resultBefore, calcNumber) {
+  const calcDescription = `${resultBefore} ${operator} ${calcNumber}`;
+  outputResult(calcNumber, calcDescription); //from vendor file
+}
+
+function writeToLog(
+  operationIdentifier,
+  prevResult,
+  operationNumber,
+  newResult
+) {
+  const logEntry = {
+    operation: operationIdentifier,
+    prevResult: prevResult,
+    number: operationNumber,
+    result: newResult,
+  };
+  logEntries.push(logEntry);
+  console.log(logEntries);
+}
+function add() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult += enteredNumber;
+  createAndWriteOutput('+', initialResult, currentResult);
+  writeToLog('ADD', initialResult, enteredNumber, currentResult);
+}
+
+function subtract() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult -= enteredNumber;
+  createAndWriteOutput('-', initialResult, currentResult);
+  writeToLog('SUBTRACT', initialResult, enteredNumber, currentResult);
+}
+
+function multiply() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult *= enteredNumber;
+  createAndWriteOutput('*', initialResult, currentResult);
+  writeToLog('MULTIPLY', initialResult, enteredNumber, currentResult);
+}
+
+function divide() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult /= enteredNumber;
+  createAndWriteOutput('/', initialResult, currentResult);
+  writeToLog('DIVIDE', initialResult, enteredNumber, currentResult);
+}
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
---
+++ 4. Working with Control Structures (if Statements, Loops, Error Handling)/Monster Killer/assets/scripts/app.js
@@ -0,0 +1,5 @@
+const multiply = (a, b) => {
+  return a / (1 / b);
+};
+
+console.log(multiply(3, 5));
