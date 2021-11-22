1ff0476685233a50b4f0cea5793938c893ae2b6f
Rafa Gayoso <gayoso0597@gmail.com>
2021-11-22 15:00:04 +0100

    FInished Object Module

--- 9. More on Objects/assets/scripts/objects.js
+++ 9. More on Objects/assets/scripts/objects.js
@@ -1,12 +1,72 @@
-const person = {
-  name: 'Max',
-  age: 30,
-  hobbies: ['Sports', 'Cooking'],
-  greet: () => {
-    alert('Hi there');
-  },
+const addMovieBtn = document.getElementById('add-movie-btn');
+const searchBtn = document.getElementById('search-btn');
+
+const movies = [];
+
+const renderMovies = (filter = '') => {
+  const movieList = document.getElementById('movie-list');
+
+  if (movies.length === 0) {
+    movieList.classList.remove('visible');
+    return;
+  } else {
+    movieList.classList.add('visible');
+  }
+  movieList.innerHTML = '';
+
+  const filteredMovies = !filter
+    ? movies
+    : movies.filter((movie) => movie.info.title.includes(filter));
+
+  filteredMovies.forEach((movie) => {
+    const movieEl = document.createElement('li');
+    const { info, ...otherProps } = movie;
+    const { title: movieTitle } = info;
+    let text = movie.getFormattedTitle() + ' - ';
+    for (const key in info) {
+      if (key !== 'title') {
+        text = text + `${key}: ${info[key]}`;
+      }
+    }
+    movieEl.textContent = text;
+    movieList.append(movieEl);
+  });
 };
 
-person.isAdmin = true;
-delete person.age;
-console.log(person);
+const addMovieHandler = () => {
+  const title = document.getElementById('title').value;
+  const extraName = document.getElementById('extra-name').value;
+  const extraValue = document.getElementById('extra-value').value;
+
+  const newMovie = {
+    info: {
+      get title() {
+        return this._title.toUpperCase();
+      },
+      set title(title) {
+        if (title.trim === '') {
+          title = 'DEFAULT';
+          return;
+        }
+        this._title = title;
+      },
+      [extraName]: extraValue,
+    },
+    id: Math.random().toString(),
+    getFormattedTitle: function () {
+      return this.info.title.toUpperCase();
+    },
+  };
+
+  newMovie.info.title = title;
+  movies.push(newMovie);
+  renderMovies();
+};
+
+const searchMovieHandler = () => {
+  const filterTerm = document.getElementById('filter-title').value;
+  renderMovies(filterTerm);
+};
+
+addMovieBtn.addEventListener('click', addMovieHandler);
+searchBtn.addEventListener('click', searchMovieHandler);
