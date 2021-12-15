cf0c5d5f580b74f1efedda41a529c2ee7986bac8
RafaGayoso1997 <rafagayoso97@gmail.com>
2021-12-15 16:22:03 +0100

    Using Inheritance Everywhere

--- 10. Class And Object Oriented Programming/assets/scripts/app.js
+++ 10. Class And Object Oriented Programming/assets/scripts/app.js
@@ -1,39 +1,38 @@
 class Product {
-  title = ''; //field
-  imageUrl; //field
-  description; //field
-  price; //field
+  // title = 'DEFAULT';
+  // imageUrl;
+  // description;
+  // price;
 
-  constructor(title, image, description, price) {
+  constructor(title, image, desc, price) {
     this.title = title;
     this.imageUrl = image;
-    this.description = description;
+    this.description = desc;
     this.price = price;
   }
 }
-
 class ElementAttribute {
   constructor(attrName, attrValue) {
     this.name = attrName;
-    this.vale = attrValue;
+    this.value = attrValue;
   }
 }
+
 class Component {
-  constructor(renderHook) {
-    this.hookId = renderHook;
+  constructor(renderHookId) {
+    this.hookId = renderHookId;
   }
+
   createRootElement(tag, cssClasses, attributes) {
     const rootElement = document.createElement(tag);
     if (cssClasses) {
       rootElement.className = cssClasses;
     }
-
     if (attributes && attributes.length > 0) {
       for (const attr of attributes) {
         rootElement.setAttribute(attr.name, attr.value);
       }
     }
-
     document.getElementById(this.hookId).append(rootElement);
     return rootElement;
   }
@@ -46,7 +45,7 @@ class ShoppingCart extends Component {
     this.items = value;
     this.totalOutput.innerHTML = `<h2>Total: \$${this.totalAmount.toFixed(
       2
-    )}<h2>`;
+    )}</h2>`;
   }
 
   get totalAmount() {
@@ -62,23 +61,24 @@ class ShoppingCart extends Component {
   }
 
   addProduct(product) {
-    const updateItems = [...this.items];
-    updateItems.push(product);
-    this.cartItems = updateItems;
+    const updatedItems = [...this.items];
+    updatedItems.push(product);
+    this.cartItems = updatedItems;
   }
 
   render() {
     const cartEl = this.createRootElement('section', 'cart');
     cartEl.innerHTML = `
-            <h2>Total: \$${0}<h2>
-            <button> Order now </button>
-        `;
+      <h2>Total: \$${0}</h2>
+      <button>Order Now!</button>
+    `;
     this.totalOutput = cartEl.querySelector('h2');
   }
 }
 
-class ProductItem {
-  constructor(product) {
+class ProductItem extends Component {
+  constructor(product, renderHookId) {
+    super(renderHookId);
     this.product = product;
   }
 
@@ -87,62 +87,60 @@ class ProductItem {
   }
 
   render() {
-    const prodEl = document.createElement('li');
-    prodEl.className = 'product-item';
+    const prodEl = this.createRootElement('li', 'product-item');
     prodEl.innerHTML = `
-            <div>
-              <image src="${this.product.imageUrl}" alt="${this.product.title}">
-              <div class="product-item__content">
-                <h2>${this.product.title}</h2>
-                <h3>\$${this.product.price}</h3>
-                <p>${this.product.description}</p>
-                <button>Add to Cart</button>
-              </div>
-            </div>
-            `;
+        <div>
+          <img src="${this.product.imageUrl}" alt="${this.product.title}" >
+          <div class="product-item__content">
+            <h2>${this.product.title}</h2>
+            <h3>\$${this.product.price}</h3>
+            <p>${this.product.description}</p>
+            <button>Add to Cart</button>
+          </div>
+        </div>
+      `;
     const addCartButton = prodEl.querySelector('button');
     addCartButton.addEventListener('click', this.addToCart.bind(this));
-    return prodEl;
   }
 }
 
-class ProductList {
+class ProductList extends Component {
   products = [
     new Product(
       'A Pillow',
-      'https://images.unsplash.com/photo-1584100936595-c0654b55a2e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGlsbG93c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
-      'A soft pillow',
+      'https://www.maxpixel.net/static/photo/2x/Soft-Pillow-Green-Decoration-Deco-Snuggle-1241878.jpg',
+      'A soft pillow!',
       19.99
     ),
     new Product(
       'A Carpet',
-      'https://images.unsplash.com/photo-1600166898405-da9535204843?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2FycGV0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
-      'A carpet which you might like - or not',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Ardabil_Carpet.jpg/397px-Ardabil_Carpet.jpg',
+      'A carpet which you might like - or not.',
       89.99
     ),
   ];
 
-  constructor() {}
+  constructor(renderHookId) {
+    super(renderHookId);
+  }
 
   render() {
-    const prodList = document.createElement('ul');
-    prodList.className = 'product-list';
+    this.createRootElement('ul', 'product-list', [
+      new ElementAttribute('id', 'prod-list'),
+    ]);
     for (const prod of this.products) {
-      const productItem = new ProductItem(prod);
-      prodList.append(productItem.render());
+      const productItem = new ProductItem(prod, 'prod-list');
+      productItem.render();
     }
-    return prodList;
   }
 }
 
 class Shop {
   render() {
-    const renderHook = document.getElementById('app');
-
     this.cart = new ShoppingCart('app');
-    const productList = new ProductList();
 
-    renderHook.append(productList.render());
+    const productList = new ProductList('app');
+    productList.render();
     this.cart.render();
   }
 }
